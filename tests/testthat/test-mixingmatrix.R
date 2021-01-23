1aa20d3e33dce4a00872bd0fb7d8b94c51fa5778
Michal Bojanowski <michal2992@gmail.com>
2021-01-23 23:54:34 +0100

    Add tests

--- tests/testthat/test-mixingmatrix.R
+++ tests/testthat/test-mixingmatrix.R
@@ -15,3 +15,27 @@ test_that("mixing matrices for FMH and egoFMH are equivalent", {
   expect_identical(is.directed(mm.ego), is.directed(mm))
   expect_identical(is.bipartite(mm.ego), is.bipartite(mm))
 })
+
+
+
+test_that("error if attribute not present", {
+  expect_error(
+    mixingmatrix(fmh.ego, "no-such-attribute")
+  )
+})
+
+
+test_that("mixing matrices for attribute with NAs", {
+  data("emon")
+  texas.ego <- as.egodata(emon$Texas)
+  expect_equivalent(
+    mm.ego <- mixingmatrix(texas.ego, "Formalization"),
+    {
+      mm <- mixingmatrix(emon$Texas, "Formalization")
+      diag(mm) <- diag(mm) * 2
+      mm
+    }
+  )
+  expect_identical(is.directed(mm.ego), is.directed(mm))
+  expect_identical(is.bipartite(mm.ego), is.bipartite(mm))
+})
